3da7b7450160827319981993cf19de05e020f420
Tomas <tomas.milkintas1@gmail.com>
2020-09-02 13:03:00 +0100

    adding react context api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-    return (
-        <div className="app">
-            <h1>Hello Future Spotify</h1>
-        </div>
-    );
+    const [{ user, token }, dispatch] = useDataLayerValue();
+
+    useEffect(() => {
+        const hash = getTokenFromUrl();
+        const _token = hash.access_token;
+
+        window.location.hash = "";
+
+        if (_token) {
+            dispatch({
+                type: "SET_TOKEN",
+                token: _token,
+            });
+
+            spotify.setAccessToken(_token);
+
+            console.log("I have a token", _token);
+            spotify.getMe().then((user) => {
+                dispatch({
+                    type: "SET_USER",
+                    user: user,
+                });
+            });
+        }
+    }, []);
+
+    return <div className="app">{token ? <Player spotify={spotify} /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/Player.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Players.css";
+
+function Player({ spotify }) {
+    return (
+        <div>
+            <h1>Welcome to spotify</h1>
+        </div>
+    );
+}
+
+export default Player;
